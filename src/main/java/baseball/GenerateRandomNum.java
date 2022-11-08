9bbd3764befb2b101626a3aac083f0219059ff6f
romanticker <romanticker54@naver.com>
2022-11-08 20:56:30 +0900

    2번째 랜덤숫자뽑기 기능

---
+++ src/main/java/baseball/GenerateRandomNum.java
@@ -0,0 +1,28 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+//import java.util.Random;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class GenerateRandomNum { //랜덤숫자뽑기 기능
+    public int randomMake(){
+        //Random random = new Random();
+        //return random.nextInt(9) + 1;
+        int rannum = Randoms.pickNumberInRange(1, 9);
+        return rannum;
+    }
+
+    public List<Integer> create(){
+        List<Integer> computerNumber = new ArrayList<>();
+        while (computerNumber.size() < 3){
+            int randomNumber = randomMake();
+            if(computerNumber.contains(randomNumber)){
+                continue;
+            }else {
+                computerNumber.add(randomNumber);
+            }
+        }
+        return computerNumber;
+    }
+}
